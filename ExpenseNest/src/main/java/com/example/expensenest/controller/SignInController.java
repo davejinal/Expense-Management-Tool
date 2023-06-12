825943ec5af24afd497e843444b430ce5c4869ef
Jinal Dave <jn270099@dal.ca>
2023-06-12 10:31:18 -0300

    Adding code files for Sign In module

--- ExpenseNest/src/main/java/com/example/expensenest/controller/SignInController.java
+++ ExpenseNest/src/main/java/com/example/expensenest/controller/SignInController.java
@@ -1,7 +1,15 @@
 package com.example.expensenest.controller;
 
-public class SignInController {
-//    @GetMapping("/signin")
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class SignInController {
+    @GetMapping("/signin")
+    public String getSignInPage (Model model) {
+        model.addAttribute("signin", null);
+        return "signin";
+    }
 
 }
